bd63d92669adb044a748453fbc37477f478f633b
MariiaSurmenok <mariasurmenok@gmail.com>
2017-10-17 18:20:12 -0700

    added google map and markers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Array of places in Redwood City, CA
 var places = [
     {
         title: 'County History Museum',
@@ -24,10 +24,47 @@ var places = [
         title: 'Blu Harbor',
         lat: 37.499957,
         lng: -122.223999
+    },
+    {
+        title: 'Redwood City DMV',
+        lat: 37.492581,
+        lng: -122.229020
+    },
+    {
+        title: 'Trader Joe\'s',
+        lat: 37.496354,
+        lng: -122.248546
+    },
+    {
+        title: 'Costco',
+        lat: 37.478404,
+        lng: -122.216502
     }
 ];
 
+// Google Map
+var map;
+var markers = [];
 
+function initMap() {
+    map = new google.maps.Map(document.querySelector('#map'), {
+        center: {lat: 37.4986064, lng: -122.229146},
+        zoom: 13
+    });
+    createMarkers(places);
+}
+
+function createMarkers(places) {
+    for(var i = 0; i < places.length; i++) {
+        var marker = new google.maps.Marker({
+            position: {lat: places[i].lat, lng: places[i].lng},
+            map: map,
+            title: places[i].title
+        });
+    }
+}
+
+// Knockout JS
 function AppViewModel() {
     this.items = ko.observableArray(places);
     this.filter = ko.observable("");
@@ -41,13 +78,8 @@ function AppViewModel() {
                var title = place.title.toLowerCase();
                return title.indexOf(filter) !== -1;
             });
+            console.log(filtered_places);
             return ko.observableArray(filtered_places);
-
-            // return ko.utils.arrayFilter(this.items(), function (item) {
-            //     var title = item.title.toLowerCase();
-            //     return title.indexOf(filter) !== -1;
-            //     //return stringStartsWith(item.title.toLowerCase(), filter);
-            // });
         }
 
     }
